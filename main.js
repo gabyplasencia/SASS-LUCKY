fd8d997ac7df44bf48881033002895a6918c2c6b
Gabriela <gabyplasencia1995@gmail.com>
2023-07-14 11:31:34 +0100

    ahora se reordenan al salir de la carta

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const cards = document.querySelectorAll(".carta");
 document.body.addEventListener("click", (e) => {
+
     if(e.target.classList.contains("carta") || e.target.closest(".carta")){return}
     cards.forEach((card) =>{
         card.classList.remove("selected")
         card.classList.remove("hidden")
     })
+    reorderCards(cards);
+
 })
+
+const reorderCards = (cards) =>{
+    const container = document.querySelector(".container")
+    container.innerHTML = ""
+    const reorderedCards = Array.from(cards).sort(() => Math.random() - 0.5);
+    reorderedCards.forEach(card => container.appendChild(card))
+
+};
+
 cards.forEach ((card) =>{
 
     card.addEventListener("click", (e) => {
